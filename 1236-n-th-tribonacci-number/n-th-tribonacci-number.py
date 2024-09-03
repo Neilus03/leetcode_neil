7aa045004e5e11e236423bfb91402259fbba22df
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2024-09-04 00:22:26 +0200

    Time: 36 ms (45.47%) | Memory: 16.5 MB (16.60%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         F=[0]*38
-        F[1] = 1
-        F[2] = 1
+        F[1], F[2] = 1, 1
         for i in range(3, len(F)):
             F[i] = F[i-1] + F[i-2] + F[i-3]
         return F[n]
