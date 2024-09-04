65004bdcda0d48a182114fffb13aaca1bdd533dc
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2024-09-04 09:30:18 +0200

    Time: 26 ms (97.29%) | Memory: 16.5 MB (80.33%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -2,13 +2,10 @@ class Solution:
     def reverse(self, x: int) -> int:
         # Reverse the integer as a string, handle the negative sign if present
         reversed_num = int(str(-x)[::-1]) if x < 0 else int(str(x)[::-1])
-        
-        # Check if the original number was negative
-        neg = x < 0
 
         # Return 0 if the reversed number is out of 32-bit integer range
         if reversed_num >= 2**31 - 1 or reversed_num < -2**31:
             return 0
 
         # Return the reversed number with the correct sign
-        return -reversed_num if neg else reversed_num
+        return -reversed_num if x < 0 else reversed_num
